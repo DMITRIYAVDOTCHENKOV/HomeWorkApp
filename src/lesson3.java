1ed8dbfb10f5b19a3addfa47af58b2d2b9e5fd28
DMITRIYAVDOTCHENKOV <dimka.a2010@yandex.ru>
2021-10-14 11:42:28 +0300

    Home_Work_3 + task7 new

--- src/lesson3.java
+++ src/lesson3.java
@@ -14,21 +14,79 @@ public class lesson3 {
         System.out.println("--------");
         minAndMax();
         System.out.println("--------");
-        System.out.println(comparingValuesInAnArray());
+        System.out.println(comparingValuesInAnArray(new int[]{4, 5, 5, 5, 5, 15, 10}));
+        System.out.println("--------");
+
+        shiftArray(new int[] {1, 2, 3, 4, 5}, 6);
+        shiftArray(new int[] {1, 2, 3, 4, 5}, 1);
+        shiftArray(new int[] {1, 2, 3, 4, 5}, 2);
+        shiftArray(new int[] {1, 2, 3, 4, 5}, 5);
+        shiftArray(new int[] {1, 2, 3, 4, 5}, 6);
+        shiftArray(new int[] {1, 2, 3, 4, 5}, -1);
+        shiftArray(new int[] {1, 2, 3, 4, 5}, -2);
+        shiftArray(new int[] {1, 2, 3, 4, 5}, -5);
+        shiftArray(new int[] {1, 2, 3, 4, 5}, -6);
+
 
     }
-
-    private static boolean comparingValuesInAnArray() {
-        int[] myArray = {4, 5, 5, 5, 5, 15, 10};
-        int sum = 0;
-        int sum1 = 0;
-        int a = myArray.length - 1;
-        int b = myArray.length - 2;
-        for (int i = 0; i < myArray.length - 2; i++) {
-            sum += myArray[i];
-            sum1 = myArray[a] + myArray[b];
+    private static void shiftArray(int[] array, int n) {
+        int shiftNumber = n % array.length;
+        if (shiftNumber < 0) {
+            shiftLeft(array, Math.abs(shiftNumber));
         }
-        return sum == sum1;
+        else {
+            shiftRight(array, shiftNumber);
+        }
+        System.out.println(Arrays.toString(array) + "; shiftNumber = " + n);
+    }
+    private static  void shiftRight(int[] array, int n) {
+        for (int i = 0; i < n ; i++) {
+            int lastValue = array[array.length - 1];
+            for (int j = array.length - 1; j > 0 ; j--) {
+                array[j] = array[j - 1];
+            }
+            array[0] = lastValue;
+        }
+    }
+    private static void shiftLeft(int[] array, int n) {
+        for (int i = 0; i < n ; i++) {
+            int firstValue = array[0];
+            for (int j = 0; j < array.length - 1; j++) {
+                array[j] = array[j + 1];
+            }
+            array[array.length - 1] = firstValue;
+
+        }
+    }
+
+    private static boolean comparingValuesInAnArray(int[] maArray) {
+        boolean result = false;
+        int lefSum = 0;
+        int rightSum = sum(maArray, 0, maArray.length);
+
+
+        for (int i = 0; i < maArray.length; i++) {
+            rightSum -= maArray[i];
+            lefSum += maArray[i];
+
+
+            if (lefSum == rightSum) {
+                System.out.println("Comon sum = " + lefSum + "; index = " + 1);
+                result = true;
+                break;
+            }
+        }
+        System.out.println("----------");
+        return result;
+    }
+
+    private static int sum(int[] maArray, int start, int end) {
+        int sum = 0;
+        for (int i = start; i < end; i++) {
+            sum += maArray[i];
+        }
+        return sum;
+
     }
 
 
@@ -48,7 +106,7 @@ public class lesson3 {
         System.out.println("аксимальный элемент: " + max);
     }
 
-    static int[] customArray(int len, int initialValue) {
+    private static int[] customArray(int len, int initialValue) {
         int[] arr = new int[len];
         Arrays.fill(arr, initialValue);
         return arr;
@@ -56,26 +114,23 @@ public class lesson3 {
 
     private static void twoDimensionalArray(int w, int h) {
         int[][] data = new int[w][h];
-        for (int i = 0; i < w; i++)
-            for (int j = 0; j < h; j++)
-                data[i][j] = 1;
-
-        for (int[] datum : data) {
-            for (int i : datum) {
-                System.out.print(i + " ");
-            }
-            System.out.println();
+        int lastIndex = data.length - 1;
+        for (int i = 0; i < data.length; i++) {
+            data[i][i] = data[i][lastIndex - 1] = 1;
+            System.out.println(Arrays.toString(data[i]));
         }
     }
 
 
     private static void arrayMultiplication() {
         int[] array = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-        for (int j : array) {
-            if (j < 6) {
-                System.out.println(j * 2);
+        for (int i = 0; i < array.length; i++) {
+            int value = array[i];
+            if (value < 6) {
+                array[i] = value * 2;
             }
         }
+        System.out.println(Arrays.toString(array));
     }
 
     private static void autoArrayUpTo100() {
@@ -88,17 +143,8 @@ public class lesson3 {
 
     private static void integerArray() {
         int[] nums = {1, 1, 0, 0, 1, 1, 0};
-        for (int i = 0; i < nums.length; i++) ;
-        System.out.println(Arrays.toString(nums));
-        System.out.println();
-        nums[0] = 0;
-        nums[1] = 0;
-        nums[2] = 1;
-        nums[3] = 1;
-        nums[4] = 0;
-        nums[5] = 0;
-        nums[6] = 1;
         for (int i = 0; i < nums.length; i++) {
+            nums[i] = ((nums[i] == 1) ? 0 : 1);
         }
         System.out.println(Arrays.toString(nums));
     }
